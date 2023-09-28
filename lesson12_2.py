41387e90669e0f46b00f5bc6827ed95a3cb30318
mercury <mercury.0601a@gmail.com>
2023-09-28 15:59:10 +0800

    modify

---
+++ lesson12_2.py
@@ -0,0 +1,30 @@
+class Player:
+    def __init__(self):
+        pass
+
+    def __play() -> int :
+        pass
+
+    @property
+    def value(self)->int:
+        #呼叫self.play()
+        pass
+
+    def __repr__(self) ->str:
+        descript = ""
+        descript +="徐國堂\n"
+        descript +="骰子1=5\n"
+        descript +="骰子2=3\n"
+        descript +="骰子3=5\n"
+        descript +="骰子4=5\n"
+        descript +="得分15分\n"
+        return descript
+if __name__ == "__main__" :
+    p1 = Player("robert")
+    print(p1.value)
+    print (p1)
+
+    p2 = Player("robert")
+    print(p2.value)
+    print (p2)
+

86e3ac8ae9242ad46f27f2e2d4c14df2b2a69597
isabel112233 <mercury.0601a@gmail.com>
2023-10-16 13:41:11 +0800

    modify

--- lesson18/index3.py
+++ lesson18/index3.py
@@ -8,30 +8,29 @@ from PIL import Image,ImageTk         #查網站安裝pillow
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        #self.geometry("300x250")
-        self.title("Images")                      
+        self.geometry("300x250")
+        self.title("Images")  
+        self.configure(background="#F4A7B9")                    
        
 
-class MyFrame(tk.Frame):
-    def __init__(self,master,**kwargs):
-        super().__init__(master,**kwargs)
-        self.configure(background='#9E7A7A')
-        self.img = Image.open("pets.png")
-        self.pets = ImageTk.PhotoImage(self.img)
-        #canvas = tk.Canvas(self,width=48,height=48)
-        #canvas.create_image(12,12,image=self.pets,anchor=tk.NW)
-        #canvas.pack()
-        petLabel = tk.Label(self,image=self.pets,width=50,height=50,background="#DDA52D")
-        petLabel.pack(padx=40,pady=40)
+class MyFrame(ttk.LabelFrame):
+    def __init__(self,master,title,**kwargs):               #自訂Class 新增加一參數title
+        super().__init__(master,text=title,**kwargs)        #繼承之calss也同時增加一參數title
+        self.aligement = tk.StringVar(value='left')
+        ttk.Radiobutton(self,text="左邊",value='left',variable=self.aligement,command=self.choised).grid(column=0,row=0,padx=10)
+        ttk.Radiobutton(self,text="中間",value='center',variable=self.aligement,command=self.choised).grid(column=1,row=0,padx=10)
+        ttk.Radiobutton(self,text="右邊",value='right',variable=self.aligement,command=self.choised).grid(column=2,row=0,padx=10)
+        self.pack(expand=1, fill='x',padx=10,pady=10)
 
-
-        self.pack(expand=1, fill='both')
+    def choised(self):
+        print(self.aligement.get())
 
 
 def main():    
     window = Window()
-    myFrame = MyFrame(window)
+    myFrame = MyFrame(window,"對齊方式")
     window.mainloop()
+
 
 if __name__ == "__main__":
     main()

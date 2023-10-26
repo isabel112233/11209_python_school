64658391ebeebc07d5c9d28bc1ab6025c4b8a451
isabel112233 <mercury.0601a@gmail.com>
2023-10-26 15:06:43 +0800

    modify

--- youbike/datasource.py
+++ youbike/datasource.py
@@ -1,7 +1,7 @@
 import requests
 import sqlite3
 
-def download_youbike_data()->list[dict]:
+def __download_youbike_data()->list[dict]:
     '''
     下載youbike資料2.0
     https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json
@@ -18,7 +18,7 @@ def download_youbike_data()->list[dict]:
     #    raise Exception("下載失敗")
 
 
-def create_table(conn):    
+def __create_table(conn:sqlite3.Connection):    
     cursor = conn.cursor()
     cursor.execute(
         '''
@@ -31,19 +31,35 @@ def create_table(conn):
             "總車輛數"	INTEGER,
             "可借"	INTEGER,
             "可還"	INTEGER,
-            PRIMARY KEY("id" AUTOINCREMENT)
+            PRIMARY KEY("id" AUTOINCREMENT),
+            UNIQUE(站點名稱,更新時間) ON CONFLICT REPLACE
         );
         '''
     )
     conn.commit()
 
-def updata_sqlite_data():
+def __insert_data(conn:sqlite3.Connection,values:list[any])->None:
+    cursor = conn.cursor()
+    
+    sql=   '''
+    REPLACE INTO 台北市youbike(站點名稱,行政區,更新時間,地址,總車輛數,可借,可還)VALUES(?,?,?,?,?,?,?)
+
+        '''
+       
+    cursor.execute(sql,values)
+    conn.commit()
+
+
+def updata_sqlite_data()->None:
     '''
     下載,並更新資料庫
     '''
-    data = download_youbike_data()
+    data = __download_youbike_data()
     conn = sqlite3.connect("youbike.db")
-    __create_teble(conn)       
+    __create_table(conn)  
+    for item in data:
+        __insert_data(conn,[item['sna'],item['sarea'],item['mday'],item['ar'],item['tot'],item['sbi'],item['bemp']])
+    conn.close()
 
 
 

8fef3a9b00bbc64b8cfce798be043a7fe0529b1c
isabel112233 <mercury.0601a@gmail.com>
2023-12-06 03:34:51 +0000

    modify

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -1,10 +1,18 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import pandas as pd
-dash2 = Dash(requests_pathname_prefix="/dash/app2/")
+import dash_bootstrap_components as dbc
 
-dash2.layout = html.Div([
-    html.H1("Dash H1"),
-    html.P("這是段茖1"),
-    html.P("這是段茖2"),
+dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-])
+
+
+dash2.layout = html.Div(
+    [html.H1("BootSrap Layout"),
+     html.P("這是段茖1"),
+     html.P("這昃段落2"),
+     ],
+     className="container-lg",
+     style={'backgroundColor':'#666'}
+
+
+    )
--- flask_dash/main.py
+++ flask_dash/main.py
@@ -4,6 +4,7 @@ from werkzeug.serving import run_simple
 from dash_file.dash_app1 import dash1
 from dash_file.dash_app2 import dash2
 
+
 app = Flask(__name__)
 
 application = DispatcherMiddleware(

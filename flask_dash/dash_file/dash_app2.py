fb8c4775576d55afb69ebdc18a8577c25dc3b812
isabel112233 <mercury.0601a@gmail.com>
2023-12-11 08:59:16 +0000

    modify

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -1,4 +1,4 @@
-from dash import Dash, html,dash_table
+from dash import Dash, html,dash_table,Input,Output,callback,dcc
 import pandas as pd
 import dash_bootstrap_components as dbc
 from . import datasource
@@ -23,33 +23,82 @@ dash2.layout = html.Div(
             style={"paddingTop":'2rem'}),
             html.Div([
                 html.Div([
+                    html.Div([
+                                dbc.Label("站點名稱"),
+                                dbc.Input(id='input_value',
+                                          placeholder="請輸入站點名稱", type="text"),                                
+                    ])
+        
+                ],className="col"),
+                html.Div([
+                    html.Button('確定', id='submit-val',className="btn btn-primary")
+                    ],className="col"),
+                html.Div(children="輸入內容",
+                         id="output-content",
+                         className="col"),
+            ],
+            className="row row-cols-auto align-items-end",
+            style={"paddingTop":'2rem'}),
+            html.Div([
+                html.Div([
                     dash_table.DataTable(
-                        id = 'main_table',
+                        id='main_table',
                         data=lastest_df1.to_dict('records'),
                         columns=[{'id':column,'name':column} for column in lastest_df1.columns],
                         page_size=20,
                         style_table={'height': '300px', 'overflowY': 'auto'},
                         fixed_rows={'headers': True},
                         style_cell_conditional=[
-                        {'if': {'column_id': 'index'},
-                            'width': '5%'},
-                        {'if': {'column_id': '站點名稱'},
-                            'width': '25%'},
-                        {'if': {'column_id': '總數'},
-                            'width': '5%'}, 
-                        {'if': {'column_id': '可借'},
-                            'width': '5%'}, 
-                        {'if': {'column_id': '可還'},
-                            'width': '5%'},            
+                                {   'if': {'column_id': 'index'},
+                                 'width': '5%'
+                                },
+                                {   'if': {'column_id': '站點名稱'},
+                                 'width': '25%'},
+                                {   'if': {'column_id': '總數'},
+                                 'width': '5%'},
+                                {   'if': {'column_id': '可借'},
+                                 'width': '5%'},
+                                {   'if': {'column_id': '可還'},
+                                 'width': '5%'},
                         ],
-                        row_selectable='single',
-                        selected_rows = [0]
+                        row_selectable="single",
+                        selected_rows=[]
                     ),
                 ],className="col text-center")
             ],
             className="row",
-            style={"paddingTop":'2rem'}),
+            style={"paddingTop":'0.5rem'}),
+            html.Div([
+                html.Div(children="",className="col",id='showMessage')
+            ],
+            className="row",
+            style={"paddingTop":'2rem'})
+
         ])
     ],
     className="container-lg"
     )
+
+@callback(
+        Output('output-content','children'),
+        Input('submit-val','n_clicks'),
+        Input('input_value','value')
+)
+def clickBtn(n_clicks:None | int,inputValue:str):
+    if n_clicks is not None:
+        #一定先檢查有沒有按button
+        print(inputValue)
+        
+
+@callback(
+      Output('showMessage','children'),
+      Input('main_table','selected_rows')  
+)
+def selectedRow(selected_rows:list[int]):
+    #取得一個站點,series
+    if len(selected_rows) != 0:
+        oneSite:pd.DataFrame = lastest_df1.iloc[[selected_rows[0]]]        
+        oneTable:dash_table.DataTable =  dash_table.DataTable(oneSite.to_dict('records'), [{"name": i, "id": i} for i in oneSite.columns])
+        return oneTable
+    
+    return None

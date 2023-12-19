e03ab31648fba67c036b78c8f06e159b43a8f282
isabel112233 <mercury.0601a@gmail.com>
2023-12-19 07:01:10 +0000

    modify

--- flask_dash/auth/auth.py
+++ flask_dash/auth/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,render_template,request,redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField,SelectField
+from wtforms.validators import DataRequired,Length
 
 blueprint_auth = Blueprint('auth', __name__,url_prefix='/auth')
 
@@ -25,3 +25,19 @@ def login():
 @blueprint_auth.route('/success')
 def success():
     return render_template('/auth/success.html')
+
+class UserRegistrationForm(FlaskForm):
+    uName = StringField("姓名",validators=[DataRequired(message="此欄必需有資料"),Length(min=2,max=20)])
+    uGender = SelectField("性別",choices=[("女","女"),("男","男"),("其它","其它")])
+@blueprint_auth.route('/registor',methods=['GET','POST'])
+def register():
+    form = UserRegistrationForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            uName = form.uName.data
+            form.uName.data = ''
+            print("姓名",uName)
+
+            uGender = form.uGender.data
+            print("性別",uGender)
+    return render_template('/auth/registor.html',form=form)

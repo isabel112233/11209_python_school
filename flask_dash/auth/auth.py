16597713440f6a0f5328788692515e5c2ec564ff
isabel112233 <mercury.0601a@gmail.com>
2023-12-19 05:58:15 +0000

    modify

--- flask_dash/auth/auth.py
+++ flask_dash/auth/auth.py
@@ -1,8 +1,27 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 blueprint_auth = Blueprint('auth', __name__,url_prefix='/auth')
 
-@blueprint_auth.route('/')
-@blueprint_auth.route('/login')
+class MyForm(FlaskForm):
+    name = StringField('姓名', validators=[DataRequired()])
+
+@blueprint_auth.route('/',methods=['GET', 'POST'])
+@blueprint_auth.route('/login',methods=['GET', 'POST'])
 def login():
-    return render_template("/auth/login.html")
+    form = MyForm()
+    if request.method == "POST" and form.validate_on_submit():     
+        if request.form['name'] == "12345" and request.form['password'] == "12345":
+            print("密碼正確")
+            return redirect("/auth/success")
+        else:
+            print("密碼錯誤")
+    
+
+    return render_template("/auth/login.html",form=form)
+
+@blueprint_auth.route('/success')
+def success():
+    return render_template('/auth/success.html')
